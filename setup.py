9df3c347ed3ebcaef23a829786272cd1899891d9
Philipp Schlipper <s6pischl@uni-bonn.de>
2023-04-22 15:12:02 +0200

    Refactor directory structure to enable proper packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="taqo_webapp",
+    version="1.0.0",
+    packages=find_packages("taqo_webapp", exclude=["*tests"]),
+    package_dir={"": "taqo_webapp"},
+    install_requires=[
+        "dash",
+        "dash_bootstrap_components",
+    ],
+    entry_points={
+        'console_scripts': [
+            'taqo = webapp:run',
+        ]
+    }
+)
